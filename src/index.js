f760721b37f184f5d3fc7d46c493be446504b7c4
Xinzi Zhou <xzhou@indeed.com>
2019-01-04 13:47:49 +0800

    Add executable.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import commander from 'commander';
 
-import { version } from '../package.json'
+import { version, description } from '../package.json'
 
 commander
     .version(version)
+    .description(description)
     .option('-d', '--directory', 'Directory of the content.')
     .parse(process.argv);
 
